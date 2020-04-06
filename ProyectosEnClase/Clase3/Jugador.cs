5f37c0e1edb8814da9048e57f8d360e34f7456e2
SantiGO055 <40776497+SantiGO055@users.noreply.github.com>
2020-04-06 19:12:33 -0300

    Add files via upload

---
+++ ProyectosEnClase/Clase3/Jugador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3
+{
+    class Jugador
+    {
+        public string nombre;
+        public Jugador()
+        {
+            nombre = "Sin nombre"; // es como hardcodear el nombre, cada vez que creo un jugador va a tener ese nombre
+        }
+        public void mover()
+        {
+
+        }
+    }
+}
---
+++ ProyectosEnClase/Clase3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero = 0;
+            Jugador jugador1 = new Jugador(); //no va a existir el objeto hasta que el programa corra. Para crearlo va nombre de la clase espacio y nombre del objeto
+            jugador1.mover(); //jugador 1 tiene metodo mover pero si jugador1 nunca se hizo new no va a tener mover por que no existe
+            
+            ConsoleKeyInfo consoleKeyInfo;
+
+            do
+            {
+                numero = 0;
+                
+                
+                while (!Console.KeyAvailable)
+                {
+                    numero++;
+                }
+
+                
+                consoleKeyInfo = Console.ReadKey();
+                Console.WriteLine(numero);
+            } while (consoleKeyInfo.KeyChar != 27);
+        }
+    }
+}
