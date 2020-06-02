19e1e3e09958a3f411f9d84199d1e0d697bc15a0
SantiGO055 <santigonzalez05@gmail.com>
2020-06-02 19:48:56 -0300

    archivos

---
+++ ProyectosEnClase/ArchivosDeTexto/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchivosDeTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\ins03\Documents\GitHub\ProgramacionYLabo2\ProyectosEnClase\ArchivosDeTexto\pruebita.txt"))
+            {
+                sw.Write("Este es el ");
+                sw.WriteLine("Encabezado para el archivo");
+                sw.WriteLine("--------------------------");
+
+                sw.Write("La fecha es: ");
+                sw.WriteLine(DateTime.Now);
+
+            }
+            //modifico el archivo
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\ins03\Documents\GitHub\ProgramacionYLabo2\ProyectosEnClase\ArchivosDeTexto\pruebita.txt",true))
+                //en el append true si modifico archivo y false para reemplazarlo
+            {
+                sw.Write("Este es el ");
+                sw.WriteLine("archivo modificado");
+                sw.WriteLine("--------------------------");
+
+                sw.Write("La fecha es: ");
+                sw.WriteLine(DateTime.Now);
+
+            }
+            //leo un archivo
+            using (StreamReader sr = new StreamReader(@"C:\Users\ins03\Documents\GitHub\ProgramacionYLabo2\ProyectosEnClase\ArchivosDeTexto\pruebita.txt"))
+                //en el append true si modifico archivo y false para reemplazarlo
+            {
+                string line;
+
+                while((line = sr.ReadLine()) != null) //el enter no es un null
+                {
+                    Console.WriteLine(line);
+                }
+                Console.ReadLine();
+                
+            }
+
+
+
+        }
+    }
+}
